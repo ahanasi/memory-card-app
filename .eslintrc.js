c9634fb5524ebb5c9d9a452e6ffda1903c99f913
XXX Ahana <ahanasi@seas.upenn.edu>
2021-12-13 00:07:11 -0800

    Fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react', 'prettier'],
+  plugins: ["react", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "func-names": "off",
   },
 };
--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ const shuffleArray = (array) => {
   return array;
 };
 
-const App = () => {
+const App = function () {
   const [score, setScore] = useState(0);
   const [hiScore, setHiScore] = useState(0);
   const [cards, setCards] = useState(shuffleArray([...images]));
 
-  let rows = cards.reduce(function (rows, key, index) {
-    return (index % 9 === 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) && rows;
-  }, []);
+  const rows = cards.reduce(
+    (rows, key, index) =>
+      (index % 9 === 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) &&
+      rows,
+    []
+  );
 
   const resetGame = () => {
     setScore(0);
@@ -42,7 +45,6 @@ const App = () => {
   };
 
   useEffect(() => {
-
     document.querySelector(".close-lost").addEventListener("click", () => {
       document.querySelector(".lost-msg").classList.add("is-hidden");
       resetGame();
@@ -52,7 +54,6 @@ const App = () => {
       document.querySelector(".win-msg").classList.add("is-hidden");
       resetGame();
     });
-
   }, []);
 
   useEffect(() => {
@@ -69,37 +70,44 @@ const App = () => {
       <article className="message win-msg is-success is-hidden">
         <div className="message-header">
           <p>You win!</p>
-          <button className="delete close-win" aria-label="delete"></button>
+          <button className="delete close-win" aria-label="delete" />
         </div>
       </article>
       <article className="message lost-msg is-warning is-hidden">
         <div className="message-header">
           <p>You lose!</p>
-          <button className="delete close-lost" aria-label="delete"></button>
+          <button className="delete close-lost" aria-label="delete" />
         </div>
       </article>
       <header className="my-3">
         <h1 className="title">Memory Card Game</h1>
-        <h2 className="subtitle">This application puts your memory to the test. You CAN NOT click on any image more than once or else your score resets to zero!</h2>
+        <h2 className="subtitle">
+          This application puts your memory to the test. You CAN NOT click on
+          any image more than once or else your score resets to zero!
+        </h2>
         <div className="scores columns is-justify-content-center">
           <p className="column is-2 is-size-5	">Score: {score}</p>
           <p className="column is-2 is-size-5	">Hi-Score: {hiScore}</p>
         </div>
       </header>
       <main className="mx-3 pt-6 columns is-flex-direction-column is-align-items-center">
-        {rows.map((row, i) => {
-          return (
-            <section className="tile column is-12 is-ancestor" key={uniqid()}>
-              {row.map((card, j) => {
-                return (
-                  <div className="tile is-parent" key={uniqid()}>
-                    <Card url={card.url} alt={card.alt} name={card.name} isClicked={card.isClicked} row={i} col={j} onClick={handleClick} />
-                  </div>
-                );
-              })}
-            </section>
-          );
-        })}
+        {rows.map((row, i) => (
+          <section className="tile column is-12 is-ancestor" key={uniqid()}>
+            {row.map((card, j) => (
+              <div className="tile is-parent" key={uniqid()}>
+                <Card
+                  url={card.url}
+                  alt={card.alt}
+                  name={card.name}
+                  isClicked={card.isClicked}
+                  row={i}
+                  col={j}
+                  onClick={handleClick}
+                />
+              </div>
+            ))}
+          </section>
+        ))}
       </main>
     </div>
   );
